b0ec02b44e25d70aea16a0d2b0b8789c7193ece7
anselmodev <anselmo.dev@gmail.com>
2022-12-18 19:36:36 -0300

    chore: update nom scripts and initial encrypt methods

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+export const encryptChar = {
+  encode(data: string, keychar: string) {
+    const getData = data;
+    const getKeychar = keychar;
+
+    console.log(getData, getKeychar);
+
+    return 'encoded';
+  },
+
+  decode(data: string, keychar: string) {
+    const getData = data;
+    const getKeychar = keychar;
+
+    console.log(getData, getKeychar);
+
+    return 'decoded';
+  },
 };
