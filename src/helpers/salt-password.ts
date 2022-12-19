2850c80ad3cc16920c1087b4ffc12fadd3d81654
anselmodev <anselmo.dev@gmail.com>
2022-12-18 22:45:32 -0300

    feat: implement keychar validation

--- src/helpers/salt-password.ts
+++ src/helpers/salt-password.ts
@@ -21,53 +21,72 @@ const saltPasswordEncode = (salt: number | string, password: string) => {
 };
 
 const saltPasswordDecode = (
-  saltFromData: string,
+  password: string,
   saltFromKeychar: string,
-  password: string
+  saltFromData?: string
 ) => {
   if (!password?.length || typeof password !== 'string') {
     throw new Error('Empty Password.');
-  } else if (!saltFromData?.length || typeof saltFromData !== 'string') {
-    throw new Error('Invalid string "saltFromData" value.');
   } else if (!saltFromKeychar?.length || typeof saltFromKeychar !== 'string') {
     throw new Error('Invalid string "saltFromKeychar" value.');
   }
 
-  const resultFromData = baseDecode(saltFromData)
+  const __resultFromKeychar = baseDecode(saltFromKeychar)
     .split('')
     .reverse()
     .join('');
 
-  const resultFromKeychar = baseDecode(saltFromKeychar)
-    .split('')
-    .reverse()
-    .join('');
+  if (saltFromData?.length) {
+    if (typeof saltFromData !== 'string') {
+      throw new Error('Invalid string "saltFromData" value.');
+    }
 
-  const prepareSaltAndPassword = {
-    fromData: resultFromData.split('.'),
-    fromKeychar: resultFromKeychar.split('.'),
-  };
+    const resultFromData = baseDecode(saltFromData)
+      .split('')
+      .reverse()
+      .join('');
 
-  if (
-    !prepareSaltAndPassword?.fromData[1] ||
-    !prepareSaltAndPassword?.fromKeychar[1]
-  ) {
-    throw new Error('Password Check Error.');
+    const prepareSaltAndPassword = {
+      fromData: resultFromData.split('.'),
+      fromKeychar: __resultFromKeychar.split('.'),
+    };
+
+    if (
+      !prepareSaltAndPassword?.fromData[1] ||
+      !prepareSaltAndPassword?.fromKeychar[1]
+    ) {
+      throw new Error('Password Check Error.');
+    }
+
+    const checkPassword = {
+      fromData: baseDecode(prepareSaltAndPassword.fromData[1]),
+      fromKeychar: baseDecode(prepareSaltAndPassword.fromKeychar[1]),
+    };
+
+    const passwordIsValid: boolean =
+      checkPassword.fromData === checkPassword.fromKeychar &&
+      checkPassword.fromData === password;
+
+    return {
+      passwordIsValid,
+      saltValue: Number(prepareSaltAndPassword.fromData[0]),
+    };
+  } else {
+    const prepareSaltAndPassword = __resultFromKeychar.split('.');
+
+    if (!prepareSaltAndPassword[1]) {
+      throw new Error('Password Check Error.');
+    }
+
+    const checkPassword = baseDecode(prepareSaltAndPassword[1]);
+
+    const passwordIsValid: boolean = checkPassword === password;
+
+    return {
+      passwordIsValid,
+      saltValue: Number(prepareSaltAndPassword[0]),
+    };
   }
-
-  const checkPassword = {
-    fromData: baseDecode(prepareSaltAndPassword.fromData[1]),
-    fromKeychar: baseDecode(prepareSaltAndPassword.fromKeychar[1]),
-  };
-
-  const passwordIsValid: boolean =
-    checkPassword.fromData === checkPassword.fromKeychar &&
-    checkPassword.fromData === password;
-
-  return {
-    passwordIsValid,
-    saltValue: Number(prepareSaltAndPassword.fromData[0]),
-  };
 };
 
 export { saltPasswordEncode, saltPasswordDecode };
--- src/lib/keychar.ts
+++ src/lib/keychar.ts
@@ -1,9 +1,17 @@
 import { patterns } from '../helpers/patterns';
 import { beginAndEndKeys } from '../helpers/begin-end-keys';
 import { secretGenerator } from '../helpers/secret-generator';
-import { saltPasswordEncode } from '../helpers/salt-password';
+import {
+  saltPasswordEncode,
+  // saltPasswordDecode,
+} from '../helpers/salt-password';
 
 const secretResult: string[] = [];
+
+const __getSaltOfKeychar = (keychar: string) => {
+  const resultCheck = (keychar.match(/-/g) || []).length;
+  return resultCheck / 3 || 0;
+};
 
 const __primaryKeyGen = (salt: number) => {
   const primaryKeys = patterns.baseKeys.split('').map((letter: string) => {
@@ -29,7 +37,7 @@ const __secondaryKeyGen = () => {
   return secondaryKeys.sort(() => Math.random() - 0.5).join('');
 };
 
-const keyCharGen = (salt: number, password: string) => {
+const keycharGen = (salt: number, password: string) => {
   if (!password?.length || typeof password !== 'string') {
     throw new Error('Empty Password.');
   }
@@ -66,6 +74,115 @@ const keyCharGen = (salt: number, password: string) => {
   };
 };
 
-const keyCharCheck = () => {};
+const keycharValidate = (keychar: string, password: string) => {
+  if (!keychar?.length || typeof keychar !== 'string') {
+    throw new Error('Invalid "keychar" value.');
+  } else if (!password?.length || typeof password !== 'string') {
+    throw new Error('Invalid "password" value.');
+  }
 
-export { keyCharCheck, keyCharGen };
+  const getPointsSeparators = 3;
+
+  const getSaltValue: any = __getSaltOfKeychar(keychar);
+
+  const baseKeysLength = patterns.baseKeys.split('')?.length;
+
+  const getBeginAndEndKeys = beginAndEndKeys(getSaltValue);
+  const saltPassword = saltPasswordEncode(getSaltValue, password);
+
+  const beginEndLength = (
+    getBeginAndEndKeys?.stringStart + getBeginAndEndKeys?.stringEnd
+  ).length;
+
+  const getSecretLength = getSaltValue * baseKeysLength;
+
+  const keysLength = (baseKeysLength + getSecretLength) * 2;
+
+  const encodedSaltPassword = saltPassword?.length;
+
+  const calculationResult =
+    beginEndLength +
+    encodedSaltPassword +
+    getSecretLength +
+    keysLength +
+    getPointsSeparators;
+
+  const cleanKeychar = keychar
+    .replace(getBeginAndEndKeys.stringStart, '')
+    .replace(getBeginAndEndKeys.stringEnd, '');
+
+  const getSaltPassword = cleanKeychar.split('.')[0];
+
+  return {
+    isValid:
+      calculationResult === keychar?.length && getSaltPassword === saltPassword,
+    encodedSaltPassword: saltPassword,
+  };
+};
+
+// const keyCharParse = (keychar: string, type: 'encode' | 'decode') => {
+//   // let getSalt: number;
+//   // let beginEndKeys: any;
+//   // let getBeginEnd: any;
+//   // let keycharResult: any = '';
+//   // let secretResult: string[] = [];
+//   // let primaryKey: any;
+//   // let secondaryKey: any;
+//   // // validar o keychar
+//   // if (keyCharCheck(keychar)) {
+//   //   getSalt = saltPasswordDecode(keychar);
+//   //   beginEndKeys = beginAndEndKeys(getSalt);
+//   //   getBeginEnd = Object.keys(beginEndKeys);
+//   // } else {
+//   //   throw new Error('Invalid Keychar!');
+//   // }
+//   // // remove BEGIN KEY and END KEY words
+//   // if (
+//   //   getBeginEnd.includes('stringStart') &&
+//   //   getBeginEnd.includes('stringEnd')
+//   // ) {
+//   //   keycharResult = keychar
+//   //     .replace(beginEndKeys.stringStart, '')
+//   //     .replace(beginEndKeys.stringEnd, '');
+//   // } else {
+//   //   throw new Error('Invalid BEGIN KEY and END KEY!');
+//   // }
+//   // // split keychar
+//   // keycharResult = keycharResult.split('.');
+//   // // convert secret string to object
+//   // if (keycharResult?.length === 3) {
+//   //   secretResult = checkSecret(keycharResult[0], getSalt);
+//   // } else {
+//   //   throw new Error('Invalid Secret Object!');
+//   // }
+//   // // replaced with commas the secret combination from keycharResult (index 1 and index 2)
+//   // if (secretResult?.length === 62) {
+//   //   const regSecret = new RegExp(secretResult.join('|'), 'g');
+//   //   primaryKey = keycharResult[1].replace(regSecret, ',');
+//   //   secondaryKey = keycharResult[2].replace(regSecret, ',');
+//   // } else {
+//   //   throw new Error('Invalid Secret Object!');
+//   // }
+//   // // split arrays and remove last index
+//   // primaryKey = primaryKey.split(',');
+//   // secondaryKey = secondaryKey.split(',');
+//   // primaryKey.pop();
+//   // secondaryKey.pop();
+//   // if (primaryKey?.length === 62 && secondaryKey?.length === 62) {
+//   //   const finalObject: any = {};
+//   //   if (type === 'encode') {
+//   //     primaryKey.forEach(
+//   //       (item: string, indx: number) => (finalObject[item] = secondaryKey[indx])
+//   //     );
+//   //   } else if (type === 'decode') {
+//   //     secondaryKey.forEach(
+//   //       (item: string, indx: number) => (finalObject[item] = primaryKey[indx])
+//   //     );
+//   //   }
+//   //   return finalObject;
+//   // } else {
+//   //   throw new Error('Invalid primary and secondary keys!');
+//   // }
+// };
+
+export { keycharValidate, keycharGen /* keyCharParse */ };
